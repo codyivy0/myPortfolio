61341b8f1deaaec2b62f33baaf984aa3f832a7c3
codyivy0 <codyivy0@gmail.com>
2024-01-20 13:41:02 -0600

    added rps

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -17,7 +17,9 @@ const Projects = () => {
           <div className={styles.infoCard}>
             <h2>Worldwise</h2>
             <p>
-              Worldwise is a robust Single Page Application using several react libraries such as router and leaflet to allow a user to navigate a map and log trips complete with date and details. 
+              Worldwise is a robust Single Page Application using several react
+              libraries such as router and leaflet to allow a user to navigate a
+              map and log trips complete with date and details.
             </p>
           </div>
         </a>
@@ -33,7 +35,8 @@ const Projects = () => {
           <div className={styles.infoCard}>
             <h2>Space Tours</h2>
             <p>
-              Multiple page application highlighting the destinations and flight crew of a space travel mission. 
+              Multiple page application highlighting the destinations and flight
+              crew of a space travel mission.
             </p>
           </div>
         </a>
@@ -48,9 +51,7 @@ const Projects = () => {
           />
           <div className={styles.infoCard}>
             <h2>The Far Away Planner</h2>
-            <p>
-              A vacation packing helper.
-            </p>
+            <p>A vacation packing helper.</p>
           </div>
         </a>
         <a
@@ -65,7 +66,9 @@ const Projects = () => {
           <div className={styles.infoCard}>
             <h2>usePopcorn</h2>
             <p>
-              Utilizing movieAPI, usePopcorn allows the user to search through a database of movies, seeing details, images and reviews. The user can also log their own rating to be saved using local storage.
+              Utilizing movieAPI, usePopcorn allows the user to search through a
+              database of movies, seeing details, images and reviews. The user
+              can also log their own rating to be saved using local storage.
             </p>
           </div>
         </a>
@@ -85,10 +88,30 @@ const Projects = () => {
           <div className={styles.infoCard}>
             <h2>Shortly</h2>
             <p>
-             A URL shortening service using tinyURL api to convert long URLs into shorter links. The site also logs and displays your previous requests and copies the short url to be shared to your clipboard with a quick button tap.
+              A URL shortening service using tinyURL api to convert long URLs
+              into shorter links. The site also logs and displays your previous
+              requests and copies the short url to be shared to your clipboard
+              with a quick button tap.
             </p>
           </div>
         </a>
+        <a
+          className={styles.projectLink}
+          href="https://janken-id.netlify.app/"
+        >
+          <img
+            className={styles.project}
+            src="/jankenSS.png"
+            alt="URL shortener with logging"
+          />
+          <div className={styles.infoCard}>
+            <h2>Rock, Paper, Scissors</h2>
+            <p>
+              A simple responsive paper, scissors, rock game that tracks scores.
+            </p>
+          </div>
+        </a>
+
         <a
           className={styles.projectLink}
           href="https://comforting-halva-5f64f3.netlify.app/"
@@ -101,7 +124,8 @@ const Projects = () => {
           <div className={styles.infoCard}>
             <h2>Advice Generator</h2>
             <p>
-              Utlizing the Advice Slip API this simple application fetches an advice slip on button press.
+              Utlizing the Advice Slip API this simple application fetches an
+              advice slip on button press.
             </p>
           </div>
         </a>
@@ -113,7 +137,9 @@ const Projects = () => {
           <div className={styles.infoCard}>
             <h2>Pokedex</h2>
             <p>
-              A favorite of my children this pokedex allows a user to search for specific pokemon by name or by number, see thier details and add them to a party for quick viewing
+              A favorite of my children this pokedex allows a user to search for
+              specific pokemon by name or by number, see thier details and add
+              them to a party for quick viewing
             </p>
           </div>
         </a>
